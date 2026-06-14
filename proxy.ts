5748bafac482d0e7bd68f3a6bf7620b68c08333f
Snizhana Tanasiv <snizok.t33@gmail.com>
2026-06-15 00:22:26 +0200

    hw

--- proxy.ts
+++ proxy.ts
@@ -1,24 +1,36 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
+import { checkSession } from "./lib/api/serverApi";
 
-export function middleware(req: NextRequest) {
-  const accessToken = req.cookies.get("accessToken")?.value;
-  const refreshToken = req.cookies.get("refreshToken")?.value;
+export async function middleware(req: NextRequest) {
+  const accessToken = req.cookies.get("accessToken");
+  const refreshToken = req.cookies.get("refreshToken");
 
-  const isAuthRoute =
+  const isAuthPage =
     req.nextUrl.pathname.startsWith("/sign-in") ||
     req.nextUrl.pathname.startsWith("/sign-up");
 
-  const isPrivateRoute =
-    req.nextUrl.pathname.startsWith("/profile") ||
-    req.nextUrl.pathname.startsWith("/notes");
+  const isPrivate =
+    req.nextUrl.pathname.startsWith("/notes") ||
+    req.nextUrl.pathname.startsWith("/profile");
 
-  if (!accessToken && !refreshToken && isPrivateRoute) {
+
+  if (!accessToken && refreshToken) {
+    await checkSession();
+  }
+
+
+  if (isPrivate && !accessToken && !refreshToken) {
     return NextResponse.redirect(new URL("/sign-in", req.url));
   }
 
-  if ((accessToken || refreshToken) && isAuthRoute) {
+
+  if (isAuthPage && accessToken) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/((?!_next|favicon.ico).*)"],
+};
