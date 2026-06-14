384fb80d1d07d9a82bf080be9e661a6ed8f52fb4
Snizhana Tanasiv <snizok.t33@gmail.com>
2026-06-15 00:23:48 +0200

    hw

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const accessToken = req.cookies.get("accessToken")?.value;
+  const refreshToken = req.cookies.get("refreshToken")?.value;
+
+  const isAuthRoute =
+    req.nextUrl.pathname.startsWith("/sign-in") ||
+    req.nextUrl.pathname.startsWith("/sign-up");
+
+  const isPrivateRoute =
+    req.nextUrl.pathname.startsWith("/profile") ||
+    req.nextUrl.pathname.startsWith("/notes");
+
+  if (!accessToken && !refreshToken && isPrivateRoute) {
+    return NextResponse.redirect(new URL("/sign-in", req.url));
+  }
+
+  if ((accessToken || refreshToken) && isAuthRoute) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return NextResponse.next();
+}
